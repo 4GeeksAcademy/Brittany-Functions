ace303fe74685890c3f88de932f44feb6ddc7401
MsPerry1 <bdperry90@gmail.com>
2025-07-14 16:59:24 +0000

    excuse generator1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["I was","I was","I was","I was","I was", ];
+let action = ["abducted","busy","trapped","abducted","lost"];
+let what = ["trying to teach","by aliens","in a parallel universe where time moves differently","in my tv","in my yard", ];
+let when = ["last week","last year","yesterday","in a time loop","day before yesterday",];
+
+function randomfrom(array)
+{
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+
+function genexcuse(){
+    const excuse = `${randomfrom(who)} ${randomfrom(action)} ${randomfrom(what)} ${randomfrom(when)}`
+    document.getElementById("excuse").textContent = excuse;
+}
+
+genexcuse()
